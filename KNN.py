- Made some changes to margin_of_error in KNN.py. -- Fixed the calculation of our Margin of Error. It was calculating the margin of error on behalf of test data, not our actual ground truth.
- Experimented a little with calculating the average euclidean distance between ground_truth variables and prediction variables.

- Added "Your Estimated Position: X,Y" label to our GUI window.

KNN.py
@@ -17,6 +17,8 @@ class KNNFromScratch:
         self.y_train = y
 
     def predict(self, X, y):
+        global predictions  # defined as global variable, to access it in estimated_location()
+
         predictions = self._predict(X, y)
         # print("Our predictions:", predictions)
 
@@ -28,37 +30,24 @@ class KNNFromScratch:
 
         y = np.array(y)
         distances = [euclidean_distance_multi(x, x_train) for x_train in self.X_train]
-        print("Our distances:", distances)
+        # print("Our distances:", distances)
         # get closest K (indices = Latin for 'plural of index')
         k_indices = np.argsort(distances)[:self.k]  # returns index number of k_indices of the distances list
         # example: distance[66] = 39.42, which is the closest euclidean distance from the distances list.
-        print("Our k_indices:", k_indices)
-        print("Our k_indices:", distances[66], distances[47], distances[25], distances[67], distances[29])
+        # print("Our k_indices:", k_indices)
+        # print("Our k_indices:", distances[66], distances[47], distances[25], distances[67], distances[29])
 
+        # save the ground truth locations
         groundtruth = y[k_indices]
-        print("Our groundtruths:", groundtruth)
+        # print("Our groundtruths:", groundtruth)
+
+        # save the k-nearest labels
         k_nearest_labels = [self.y_train[i] for i in k_indices]
 
         return k_nearest_labels
 
-    def root_mean_squared_error(self, predicted_value, observed_value, number_of_samples):
-        # calculate the RMSE.
-        # Formula: √Σ(Pi – Oi)2 / n
-        # where Pi is the predicted value for the i-th observation in the dataset
-        # where Oi is the observed value for the i-th observation in the dataset
-        # where n is the sample size
-        return np.sqrt((np.sum(predicted_value - observed_value) ** 2 / number_of_samples))
-
-
-    def margin_of_error(self, test, prediction):
-        # TODO: Calculate the average distance between the test-point and the true-position value.
-        # find euclidean distance from test_point to each index in k_indices
-        # print("Our test data:", test)
-        margin_of_error = euclidean_distance(test, prediction)
-        # print("Our MoE:", margin_of_error)
-        return margin_of_error
-
     def ground_truth(self):
+        ground_truth = groundtruth
         groundtruthx, groundtruthy = np.split(groundtruth, 2, axis=1)
 
         # flatten the 2-D list to a 1-D list
@@ -70,6 +59,48 @@ class KNNFromScratch:
         avg_y = sum(groundtruthy) / len(groundtruthy)
 
         # create list to save the average ground truth for the X coordinate and Y coordinate
-        avg_x_y = [avg_x, avg_y]
+        avg_XY = [avg_x, avg_y]
 
-        return avg_x_y
+        return ground_truth
+
+    def margin_of_error(self, test, prediction):
+        # TODO: Find euclidean distance from the ground_truth to the average of each index in prediction list
+        # calculate the margin of error
+        margin_of_error = euclidean_distance(test, prediction)
+
+        # Don't mind this section:
+        prediction = np.array(predictions)
+        ground_truth = groundtruth
+
+        print("prediction list:", prediction)
+        print("ground_truth list:", ground_truth)
+
+        average_euclidean_distance_singular = np.average(
+            [np.sqrt((ground_truth[0][0] - prediction[0][0]) ** 2 +
+                     (ground_truth[0][1] - prediction[0][1]) ** 2)])
+
+        # FIXME: needs to be converted into a for-loop will make it dynamic instead of depending on the array
+        #  length being 5.
+        average_euclidean_distance = np.average(
+            # first row
+            [np.sqrt((ground_truth[0][0] - prediction[0][0]) ** 2 +
+                     (ground_truth[0][1] - prediction[0][1]) ** 2),
+             # second row
+             np.sqrt((ground_truth[1][0] - prediction[1][0]) ** 2 +
+                     (ground_truth[1][1] - prediction[1][1]) ** 2),
+             # third row
+             np.sqrt((ground_truth[2][0] - prediction[2][0]) ** 2 +
+                     (ground_truth[2][1] - prediction[2][1]) ** 2),
+             # fourth row
+             np.sqrt((ground_truth[3][0] - prediction[3][0]) ** 2 +
+                     (ground_truth[3][1] - prediction[3][1]) ** 2),
+             # fifth row
+             np.sqrt((ground_truth[4][0] - prediction[4][0]) ** 2 +
+                     (ground_truth[4][1] - prediction[4][1]) ** 2)]
+        )
+
+        print("average euclidean distance between two points:", average_euclidean_distance_singular)
+        print("average euclidean distance between multiple points:", average_euclidean_distance)
+        # end of section
+
+        return margin_of_error